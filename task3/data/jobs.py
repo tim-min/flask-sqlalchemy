4b7a945695905212f907b9517031b11c8acb9f83
tim-min <94795783+tim-min@users.noreply.github.com>
2022-02-26 18:45:42 +0300

    task3

---
+++ task3/data/jobs.py
@@ -0,0 +1,19 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    end_date = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
+    user = orm.relation('Users')
